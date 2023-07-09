b44fd56714bf91c179ab5c5f05fe663c8bccfbed
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-07-09 15:53:44 +0330

    developed Provided services in iran section

--- components/ProvidedServices/ProvidedServices.tsx
+++ components/ProvidedServices/ProvidedServices.tsx
@@ -24,8 +24,10 @@ const ProvidedServices = () => {
     ];
 
     return (
-        <div className='ProvidedServices my-16 px-8'>
-            <div className='flex flex-col items-center'>
+        <div className='ProvidedServices my-16 px-8'
+            style={{ background: '#2C2929', padding: '235px 0px 120px 0px', transform: 'skewY(-6deg)', margin: '140px 0px' }}
+        >
+            <div className='flex flex-col items-center skew-y-6'>
                 <h3 className='text-white text-5xl'> ارائه خدمات در سراسر ایران </h3>
                 <p
                     className='text-white text-3xl mt-6 text-center'
@@ -36,13 +38,15 @@ const ProvidedServices = () => {
                 </p>
             </div>
 
-            <video src={require('../../assets/Videos/country.webm')} autoPlay loop muted style={{ marginTop: '-110px' }} />
+            <video src={require('../../assets/Videos/country.webm')}
+            className='skew-y-6'
+            autoPlay loop muted style={{ marginTop: '-110px' }} />
 
-            <div className='ProvidedServices__features flex flex-row-reverse'>
+            <div className='ProvidedServices__features flex flex-row-reverse justify-around items-baseline skew-y-6'>
                 {features.map((feature, index) => (
                     <div className='ProvidedServices__features__item flex flex-col items-center' key={index}>
                         <Image src={feature.icon} alt='tick' />
-                        <p className='text-white'>{feature.text}</p>
+                        <p style={{ color: '#EBDAB2' }} className='mt-2 text-base'>{feature.text}</p>
                     </div>
                 ))}
             </div>
