edb9fe2f730f65ffe20868cb88df864967c27ad3
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-07-10 17:09:22 +0330

    fixed and developed contact us section

--- components/ContactUs/ContactUs.tsx
+++ components/ContactUs/ContactUs.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ContactUsButton from './ContactUsButton/ContactUsButton';
+import pseudo from '../../assets/Images/pseudo.png'
+import Image from 'next/image';
 
 const ContactUs = () => {
     return (
-        <div className='ContactUs my-12 flex flex-row items-center justify-around	'>
+        <div className='ContactUs flex flex-row z-0 relative items-center justify-around py-10 mt-32'
+            style={{ background: '#2C2929' }}
+        >
+            <Image src={pseudo} alt='pseudo' className='absolute'
+                style={{ zIndex: '-1', width: '1500px', height: '260px' , bottom : '3px' }} />
             <div className='text-white text-right'>
                 <h3 className='text-4xl mb-6'> با ما برای کسب و کار خود در ارتباط باشید </h3>
                 <h4 className='text-lg'> با ما در ارتباط باشید تا از بهترین مشاوره ها بهره مند شوید </h4>
