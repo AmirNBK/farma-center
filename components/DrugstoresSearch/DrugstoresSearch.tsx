2b5f2d63d9c6f6eb3848c5e1f7950d13d3d56f03
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-07-15 15:44:25 +0330

    developing the drugstore search results

--- components/DrugstoresSearch/DrugstoresSearch.tsx
+++ components/DrugstoresSearch/DrugstoresSearch.tsx
@@ -12,9 +12,11 @@ const DrugstoresSearch = () => {
             <h3 className='text-white absolute left-2/4 text-3xl top-2/4'
                 style={{ transform: 'translate(-50%, -50%)' }}
             > جستجو کنید. پیدا کنید . اقدام کنید </h3>
-            <div className="p-input-icon-right top-3/4 w-2/5 left-2/4" style={{ position: 'absolute', transform: 'translate(-50%, -50%)' }}>
+            <div className="p-input-icon-right top-3/4 w-2/5 left-2/4 py-20" style={{ position: 'absolute', transform: 'translate(-50%, -50%)' }}>
                 <i className="pi pi-search" />
-                <InputText placeholder="اسم داروخانه موردنظر خود را وارد نمایید" className='w-full text-right' />
+                <InputText placeholder="اسم داروخانه موردنظر خود را وارد نمایید" className='w-full text-right'
+                style={{padding : '20px 2.5rem'}}
+                />
             </div>
         </div>
     );
--- pages/drugstores/index.tsx
+++ pages/drugstores/index.tsx
@@ -10,16 +10,43 @@ import PoliciesSection from '@/components/PoliciesSection/PoliciesSection';
 import companyPic2 from '../../assets/Images/companyPic2.jpeg'
 import { Button } from 'primereact/button';
 import DrugstoresSearch from '@/components/DrugstoresSearch/DrugstoresSearch';
+import { Card } from 'primereact/card';
 
 const vazir = Vazirmatn({ subsets: ['latin'] })
 export default function AboutUs() {
 
+    const header = (
+        <img alt="Card" src="https://primefaces.org/cdn/primereact/images/usercard.png" />
+    );
+    const footer = (
+        <div className="flex flex-wrap justify-content-end gap-2">
+            <Button label="Save" icon="pi pi-check" />
+            <Button label="Cancel" icon="pi pi-times" className="p-button-outlined p-button-secondary" />
+        </div>
+    );
     return (
         <div className={`flex min-h-screen flex-col items-center AboutUs mt-15 ${vazir.className}`}
             style={{ background: '#313232' }}
         >
             <Header HomePageHeader={false} />
             <DrugstoresSearch />
+            <div className="card flex searchResults w-96 justify-content-center my-16">
+                <Card title="داروخانه زنجان" subTitle="فروش داروخانه زنجان" footer={footer} header={header} className="text-right">
+                    <p className="m-0">
+                        واگذاري "ارزش عرفي" داروخانه زنجان ، اطراف ابهر
+                        کد : DF1261
+                        نوع سند : 60 متر تجاري
+                        وديعه : 30 ميليون
+                        اجاره : 3 ميليون
+                        مکان تقريبي : اطراف ابهر
+                        دپو دارو : در حد عرف
+                        ميزان فروش روزانه : 30 ميليون
+                        ميزان بيمه ماهانه : 70-60 ميليون
+                        نکات : دو پزشک عمومي در نزديکي -پتانسيل به دليل امکان قرارداد با اقمار
+                        ارزش عرفي داروخانه : 3 ميليارد
+                    </p>
+                </Card>
+            </div>
         </div>
     )
 }
