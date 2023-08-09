bd27c831ed3835f2f4e88d358c5e1af913052723
AmirHossein Norbakhsh <amircr7h@icloud.com>
2023-08-09 11:58:39 +0330

    fixed contact us typesript issues

--- components/ContactUs/ContactUs.tsx
+++ components/ContactUs/ContactUs.tsx
@@ -3,8 +3,14 @@ import ContactUsButton from './ContactUsButton/ContactUsButton';
 import pseudo from '../../assets/Images/pseudo.png'
 import Image from 'next/image';
 
-const ContactUs = (props : {
-    data : []
+interface ContactUsData {
+    title: string;
+    description: string;
+    buttonLabel: string;
+}
+
+const ContactUs = (props: {
+    data: ContactUsData
 }) => {
     const data = props.data
     return (
@@ -12,7 +18,7 @@ const ContactUs = (props : {
             style={{ background: '#2C2929' }}
         >
             <Image src={pseudo} alt='pseudo' className='absolute'
-                style={{ zIndex: '-1', width: '1500px', height: '260px' , bottom : '3px' }} />
+                style={{ zIndex: '-1', width: '1500px', height: '260px', bottom: '3px' }} />
             <div className='text-white text-center md:text-right px-5'>
                 <h3 className='mb-6 text-2xl sm:text-3xl md:text-4xl'> {data.title} </h3>
                 <h4 className='text-sm sm:text-base md:text-lg'> {data.description} </h4>
